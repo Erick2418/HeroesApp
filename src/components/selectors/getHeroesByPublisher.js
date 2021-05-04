ccc602735773d7d985d9ead4fbf50e45f5499fa0
Erick2418 <erickjoellimon@outlook.com>
2021-05-04 15:24:33 -0500

    cards con info de Heroes

---
+++ src/components/selectors/getHeroesByPublisher.js
@@ -0,0 +1,13 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByPublisher=(publisher)=>{
+    /*Filtro para que solo se muestren los de Dc o Marvel.. caso constrario dara error */
+    const validPublishers=['DC Comics','Marvel Comics'];
+   
+    if( !validPublishers.includes(publisher) ){
+        throw new Error(`Publisher "${publisher}" no es correcto `);
+    }
+   
+    return heroes.filter(hero=> hero.publisher=== publisher);
+
+}
---
+++ src/components/selectors/getHeroesByid.js
@@ -0,0 +1,7 @@
+import { heroes } from "../data/heroes";
+
+export const getHeroesByid=(id)=>{
+/*Filtro los heroes que el primero que aparezca lo muestra.*/
+    return heroes.find(hero=> hero.id=== id);
+
+}
