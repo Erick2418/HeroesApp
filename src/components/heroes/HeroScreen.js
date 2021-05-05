e829070d593552bbc7aa3c4ca0cf5ec802a35e1c
Erick2418 <erickjoellimon@outlook.com>
2021-05-05 13:49:22 -0500

    UseParam, extrae los parametros del link

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroesByid } from '../selectors/getHeroesByid';
 
 export const HeroScreen = () => {
+
+   // const params= useParams();// Extrae los parametros del link como objeto
+   const {heroeId}=useParams(); 
+   
+   const hero= getHeroesByid(heroeId);
+
+    if(!hero){// vvalido si no existe heroe
+        return (
+            <Redirect to="/"/>
+        )     
+    }
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+       }=hero
+       
     return (
         <div>
             <h1>Hero Screen</h1>
